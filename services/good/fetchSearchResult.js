025641301c83c601c5e978fda12b98e95af9b550
boon <qxxpdada@outlook.com>
2025-09-12 22:41:45 +0800

    fix: 格式化代码

--- services/good/fetchSearchResult.js
+++ services/good/fetchSearchResult.js
@@ -5,66 +5,66 @@ import { searchGoods } from '../../utils/api/goods';
 // TODO: 统一import风格
 /** 获取搜索结果 */
 function mockSearchResult(params) {
-  const { delay } = require('../_utils/delay');
-  const { getMockSearchResult } = require('../../model/search');
+    const { delay } = require('../_utils/delay');
+    const { getMockSearchResult } = require('../../model/search');
 
-  const data = getMockSearchResult(params);
-  console.log('mockSearchResult params:', params);
+    const data = getMockSearchResult(params);
+    console.log('mockSearchResult params:', params);
 
-  if (data.spuList.length) {
-    data.spuList.forEach((item) => {
-      item.spuId = item.spuId;
-      item.thumb = item.primaryImage;
-      item.title = item.title;
-      item.price = item.minSalePrice;
-      item.originPrice = item.maxLinePrice;
-      if (item.spuTagList) {
-        item.tags = item.spuTagList.map((tag) => ({ title: tag.title }));
-      } else {
-        item.tags = [];
-      }
+    if (data.spuList.length) {
+        data.spuList.forEach((item) => {
+            item.spuId = item.spuId;
+            item.thumb = item.primaryImage;
+            item.title = item.title;
+            item.price = item.minSalePrice;
+            item.originPrice = item.maxLinePrice;
+            if (item.spuTagList) {
+                item.tags = item.spuTagList.map((tag) => ({ title: tag.title }));
+            } else {
+                item.tags = [];
+            }
+        });
+    }
+    return delay().then(() => {
+        return data;
     });
-  }
-  return delay().then(() => {
-    return data;
-  });
 }
 
 /** 真实接口 */
 async function realSearchResult(params) {
-  const { delay } = require('../_utils/delay');
-  const { getSearchResult } = require('../../model/search');
+    const { delay } = require('../_utils/delay');
+    const { getSearchResult } = require('../../model/search');
 
-  const data = await getSearchResult(params);
-  console.log('realSearchResult params:', params);
+    const data = await getSearchResult(params);
+    console.log('realSearchResult params:', params);
 
-  if (data.spuList.length) {
-    data.spuList.forEach((item) => {
-      item.spuId = item.spuId;
-      item.thumb = item.primaryImage;
-      item.title = item.title;
-      item.price = item.minSalePrice;
-      item.originPrice = item.maxLinePrice;
-      if (item.spuTagList) {
-        item.tags = item.spuTagList.map((tag) => ({ title: tag.title }));
-      } else {
-        item.tags = [];
-      }
+    if (data.spuList.length) {
+        data.spuList.forEach((item) => {
+            item.spuId = item.spuId;
+            item.thumb = item.primaryImage;
+            item.title = item.title;
+            item.price = item.minSalePrice;
+            item.originPrice = item.maxLinePrice;
+            if (item.spuTagList) {
+                item.tags = item.spuTagList.map((tag) => ({ title: tag.title }));
+            } else {
+                item.tags = [];
+            }
+        });
+    }
+    console.log('realSearchResult data:', data);
+    return delay().then(() => {
+        return data;
     });
-  }
-  console.log('realSearchResult data:', data);
-  return delay().then(() => {
-    return data;
-  });
 }
 
 /** 获取搜索结果 */
 export function getSearchResult(params) {
-  if (false) {
-    return mockSearchResult(params);
-  }
-  return new Promise((resolve) => {
-    realSearchResult(params).then(resolve)
-  });
+    if (false) {
+        return mockSearchResult(params);
+    }
+    return new Promise((resolve) => {
+        realSearchResult(params).then(resolve)
+    });
 }
 
