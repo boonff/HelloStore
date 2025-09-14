8874c0b81268476f1d968ba198b5ad06e0274158
boon <qxxpdada@outlook.com>
2025-09-14 21:01:27 +0800

    fix:格式化代码

--- services/good/fetchGoodsDetailsComments.js
+++ services/good/fetchGoodsDetailsComments.js
@@ -1,37 +1,35 @@
-import { config } from '../../config/index';
-
 /** 获取商品详情页评论数 */
 function mockFetchGoodDetailsCommentsCount(spuId = 0) {
-  const { delay } = require('../_utils/delay');
-  const {
-    getGoodsDetailsCommentsCount,
-  } = require('../../model/detailsComments');
-  return delay().then(() => getGoodsDetailsCommentsCount(spuId));
+    const { delay } = require('../_utils/delay');
+    const { getMockGoodsDetailsCommentsCount, } = require('../../model/detailsComments');
+    return delay().then(() => getMockGoodsDetailsCommentsCount(spuId));
 }
 
 /** 获取商品详情页评论数 */
 export function getGoodsDetailsCommentsCount(spuId = 0) {
-  if (config.useMock) {
-    return mockFetchGoodDetailsCommentsCount(spuId);
-  }
-  return new Promise((resolve) => {
-    resolve('real api');
-  });
+    const { getGoodsDetailsCommentsCount } = require('../../model/detailsComments');
+    if (false) {
+        return mockFetchGoodDetailsCommentsCount(spuId);
+    }
+    return new Promise((resolve) => {
+        getGoodsDetailsCommentsCount(spuId).then(resolve)
+    });
 }
 
 /** 获取商品详情页评论 */
 function mockFetchGoodDetailsCommentList(spuId = 0) {
-  const { delay } = require('../_utils/delay');
-  const { getGoodsDetailsComments } = require('../../model/detailsComments');
-  return delay().then(() => getGoodsDetailsComments(spuId));
+    const { delay } = require('../_utils/delay');
+    const { getMockGoodsDetailsComments } = require('../../model/detailsComments');
+    return delay().then(() => getMockGoodsDetailsComments(spuId));
 }
 
 /** 获取商品详情页评论 */
 export function getGoodsDetailsCommentList(spuId = 0) {
-  if (config.useMock) {
-    return mockFetchGoodDetailsCommentList(spuId);
-  }
-  return new Promise((resolve) => {
-    resolve('real api');
-  });
+    const { getGoodsDetailsComments } = require('../../model/detailsComments');
+    if (false) {
+        return mockFetchGoodDetailsCommentList(spuId);
+    }
+    return new Promise((resolve) => {
+        getGoodsDetailsComments(spuId).then(resolve)
+    });
 }
