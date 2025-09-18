aa3a656faa92aaf3283aa7907ba30a2ad1a70bee
boon <qxxpdada@outlook.com>
2025-09-18 10:39:31 +0800

    refactor: 优化获取个人中心信息的逻辑，移除模拟数据处理

--- services/usercenter/fetchPerson.js
+++ services/usercenter/fetchPerson.js
@@ -1,28 +1,30 @@
 import { config } from '../../config/index';
+import { getUserInfo } from '../../model/usercenter';
 
 /** 获取个人中心信息 */
 function mockFetchPerson() {
-  const { delay } = require('../_utils/delay');
-  const { genSimpleUserInfo } = require('../../model/usercenter');
-  const { genAddress } = require('../../model/address');
-  const address = genAddress();
-  return delay().then(() => ({
-    ...genSimpleUserInfo(),
-    address: {
-      provinceName: address.provinceName,
-      provinceCode: address.provinceCode,
-      cityName: address.cityName,
-      cityCode: address.cityCode,
-    },
-  }));
+    const { delay } = require('../_utils/delay');
+    const { genSimpleUserInfo } = require('../../model/usercenter');
+    const { genAddress } = require('../../model/address');
+    const address = genAddress();
+    return delay().then(() => ({
+        ...genSimpleUserInfo(),
+        address: {
+            provinceName: address.provinceName,
+            provinceCode: address.provinceCode,
+            cityName: address.cityName,
+            cityCode: address.cityCode,
+        },
+    }));
 }
 
 /** 获取个人中心信息 */
-export function fetchPerson() {
-  if (config.useMock) {
-    return mockFetchPerson();
-  }
-  return new Promise((resolve) => {
-    resolve('real api');
-  });
+export async function fetchPerson() {
+    try {
+        const res = await getUserInfo()
+        return res.data
+    } catch (err) {
+        console.error('获取个人信息失败', err)
+        throw err
+    }
 }
