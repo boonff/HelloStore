ad68c482543478d6d7fefc20d93e850c204cced5
boon <qxxpdada@outlook.com>
2025-09-18 10:38:42 +0800

    refactor: 移除初始化云开发的代码，简化应用启动逻辑

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
 import updateManager from './common/updateManager';
 
 App({
-        onLaunch: function () {
-                // 初始化云开发
-                if (!wx.cloud) {
-                        console.error('请使用 2.2.3 或以上基础库');
-                } else {
-                        wx.cloud.init({
-                                env: 'cloud1-6gze4etba64f388a', // 替换为你的环境ID
-                                traceUser: true, // 是否记录用户访问
-                        });
-                }
-        },
-        onShow: function () {
-                updateManager();
-        },
+    onShow: function () {
+        updateManager();
+    },
 });
