01ce0e817f49f35441c30e8b9bdf455c33b79097
boon <qxxpdada@outlook.com>
2025-10-03 16:17:40 +0800

    feat:添加获取购物车数据的接口

--- services/cart/cart.js
+++ services/cart/cart.js
@@ -1,20 +1,15 @@
 import { config } from '../../config/index';
-
+import { CartApi } from '../../utils/api/cart'
 /** 获取购物车mock数据 */
 function mockFetchCartGroupData(params) {
-  const { delay } = require('../_utils/delay');
-  const { genCartGroupData } = require('../../model/cart');
+    const { delay } = require('../_utils/delay');
+    const { genCartGroupData } = require('../../model/cart');
 
-  return delay().then(() => genCartGroupData(params));
+    return delay().then(() => genCartGroupData(params));
 }
 
-/** 获取购物车数据 */
-export function fetchCartGroupData(params) {
-  if (config.useMock) {
-    return mockFetchCartGroupData(params);
-  }
 
-  return new Promise((resolve) => {
-    resolve('real api');
-  });
+/** 获取购物车数据 */
+export async function fetchCartGroupData() {
+    return await CartApi.getCart()
 }
