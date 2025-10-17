eb4dcde7876387ce1e04ff74278563cafcbdd4d7
boon <qxxpdada@outlook.com>
2025-10-17 10:17:12 +0800

    chore:格式化代码

--- pages/cart/components/cart-bar/index.js
+++ pages/cart/components/cart-bar/index.js
@@ -1,59 +1,59 @@
 Component({
-  options: {
-    addGlobalClass: true,
-  },
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-    isAllSelected: {
-      type: Boolean,
-      value: false,
+    options: {
+        addGlobalClass: true,
     },
-    totalAmount: {
-      type: Number,
-      value: 1,
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        isAllSelected: {
+            type: Boolean,
+            value: false,
+        },
+        totalAmount: {
+            type: Number,
+            value: 1,
+        },
+        totalGoodsNum: {
+            type: Number,
+            value: 0,
+            observer(num) {
+                const isDisabled = num == 0;
+                setTimeout(() => {
+                    this.setData({
+                        isDisabled,
+                    });
+                });
+            },
+        },
+        totalDiscountAmount: {
+            type: Number,
+            value: 0,
+        },
+        bottomHeight: {
+            type: Number,
+            value: 100,
+        },
+        fixed: Boolean,
     },
-    totalGoodsNum: {
-      type: Number,
-      value: 0,
-      observer(num) {
-        const isDisabled = num == 0;
-        setTimeout(() => {
-          this.setData({
-            isDisabled,
-          });
-        });
-      },
-    },
-    totalDiscountAmount: {
-      type: Number,
-      value: 0,
-    },
-    bottomHeight: {
-      type: Number,
-      value: 100,
-    },
-    fixed: Boolean,
-  },
-  data: {
-    isDisabled: false,
-  },
-
-  methods: {
-    handleSelectAll() {
-      const { isAllSelected } = this.data;
-      this.setData({
-        isAllSelected: !isAllSelected,
-      });
-      this.triggerEvent('handleSelectAll', {
-        isAllSelected: isAllSelected,
-      });
+    data: {
+        isDisabled: false,
     },
 
-    handleToSettle() {
-      if (this.data.isDisabled) return;
-      this.triggerEvent('handleToSettle');
+    methods: {
+        handleSelectAll() {
+            const { isAllSelected } = this.data;
+            this.setData({
+                isAllSelected: !isAllSelected,
+            });
+            this.triggerEvent('handleSelectAll', {
+                isAllSelected: isAllSelected,
+            });
+        },
+
+        handleToSettle() {
+            if (this.data.isDisabled) return;
+            this.triggerEvent('handleToSettle');
+        },
     },
-  },
 });
